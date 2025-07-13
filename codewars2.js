8720174d744c54e9f39eb2ce70da5e2d019e75a9
Alexis Bustos <alexisbustos10@yahoo.com>
2025-07-13 13:25:52 -0700

    solved codewars challenge to map every letter of a string to its position in the alphabet

--- codewars2.js
+++ codewars2.js
@@ -34,8 +34,40 @@ Examples
 "This is an example!" ==> "sihT si na !elpmaxe"
 "double  spaces"      ==> "elbuod  secaps"
 */
-const reverseWords = str => {
-  return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+// const reverseWords = str => {
+//   return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+// }
+
+// console.log(reverseWords("This is an example!"))
+
+
+/*
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in 
+the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+Input = "The sunset sets at twelve o' clock."
+Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+*/
+const alphabetPosition = text => {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let positions = []
+  let temp = text.toLowerCase().split('')
+  temp.forEach(letter => {
+    const index = alphabet.indexOf(letter);
+    if(index === -1){
+      return
+    } else {
+      positions.push(index + 1)
+    }
+  })
+  return positions.join(' ')
 }
 
-console.log(reverseWords("This is an example!"))
+console.log(alphabetPosition("The sunset sets at twelve o' clock."))
