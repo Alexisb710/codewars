fb098e93c79aa8b57880a9dd14173c5116ed1227
Alexis Bustos <alexisbustos10@yahoo.com>
2025-07-17 16:03:53 -0700

    completed codewars challenge to find needle in an array

--- codewars6.js
+++ codewars6.js
@@ -141,8 +141,33 @@ function binToInt(arr){
 const binaryArrayToNumber = arr => {
   return parseInt(arr.join(""), 2)
 }
-console.log(binToInt([0, 0, 0, 1])) // 1
-console.log(binToInt([1, 0, 1, 0])) // 10
-console.log(binToInt([0, 1, 1, 0])) // 6
-console.log(binToInt([1, 1, 1, 1])) // 15
-console.log(binToInt([1, 0, 1, 1])) // 11
+// console.log(binToInt([0, 0, 0, 1])) // 1
+// console.log(binToInt([1, 0, 1, 0])) // 10
+// console.log(binToInt([0, 1, 1, 0])) // 6
+// console.log(binToInt([1, 1, 1, 1])) // 15
+// console.log(binToInt([1, 0, 1, 1])) // 11
+
+
+/*
+[8 kyu]
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at 
+position 5" 
+Note: In COBOL, it should return "found the needle at position 6"
+*/
+function findNeedle(arr){
+  let position = arr.indexOf("needle")
+  if(position !== -1){
+    return `found the needle at position ${position}`
+  }
+}
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
