74c1819203bd70e53722102b8e95645be74be275
Alexis Bustos <alexisbustos10@yahoo.com>
2025-07-16 22:54:41 -0700

    completed codewars challenge that returns the min and max of an array/list

--- codewars5.js
+++ codewars5.js
@@ -105,4 +105,23 @@ const totalPointss = scores => {
     }
   }).reduce((acc, val)=> acc + val, 0)
 }
-console.log(totalPointss(["3:1", "2:2", "0:1"]))
+// console.log(totalPointss(["3:1", "2:2", "0:1"]))
+
+/*
+[7 kyu]
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples (Input --> Output)
+[1,2,3,4,5] --> [1,5]
+[2334454,5] --> [5,2334454]
+[1]         --> [1,1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. 
+Also, your function will always get an array or a list, you don't have to check for null, 
+undefined or similar.
+*/
+function minMax(arr){
+  let min = Math.min(...arr)
+  let max = Math.max(...arr)
+  return [min,max]; // fix me!
+}
