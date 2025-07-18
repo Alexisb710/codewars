a3587e5206efb4fa9b52c4a0b0ff9d35bc6ad78e
Alexis Bustos <alexisbustos10@yahoo.com>
2025-07-17 20:21:13 -0700

    completed codewars challenges that prints string of people who liked a post

--- codewars6.js
+++ codewars6.js
@@ -189,8 +189,88 @@ removeChar('xyz')      // -> 'y'
 function removeChar(str) {
   return str.slice(1,-1)
 }
-removeChar('eloquent') // -> 'loquen'
-removeChar('country')  // -> 'ountr' 
-removeChar('person')   // -> 'erso'
-removeChar('ab')       // -> '' (empty string)
-removeChar('xyz')      // -> 'y'
+// removeChar('eloquent') // -> 'loquen'
+// removeChar('country')  // -> 'ountr' 
+// removeChar('person')   // -> 'erso'
+// removeChar('ab')       // -> '' (empty string)
+// removeChar('xyz')      // -> 'y'
+
+/*
+[7 kyu]
+Description:
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and 
+carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function 
+receives one side of the DNA (string, except for Haskell); you need to return 
+the other complementary side. DNA strand is never empty or there is no DNA at 
+all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+*/
+
+// function dna(strand){
+//   let temp = strand.split('')
+//   let string = []
+//   for(let letter of temp){
+//     if(letter == 'A'){
+//       string.push('T')
+//     } else if(letter == 'T'){
+//       string.push('A')
+//     } else if(letter == 'C'){
+//       string.push('G')
+//     } else if(letter == 'G'){
+//       string.push('C')
+//     }
+//   }
+//   return string.join('')
+// }
+
+// REFACTOR
+function dna(strand) {
+  const pairs = { A: 'T', T: 'A', C: 'G', G: 'C' };
+  return strand.split('').map(letter => pairs[letter]).join('');
+}
+
+
+// console.log(dna("ATTGC")) // -> TAACG
+// console.log(dna("GTAT")) // -> CATA
+
+/*
+You probably know the "like" system from Facebook and other pages. People can 
+"like" blog posts, pictures or other items. We want to create the text that 
+should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that 
+like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/
+function likes(people){
+  if(people.length == 0){
+    return 'no one likes this'
+  } else if(people.length == 1){
+    return `${people[0]} likes this`
+  } else if(people.length === 2){
+    return `${people[0]} and ${people[1]} like this`
+  } else if(people.length === 3){
+    return `${people[0]}, ${people[1]} and ${people[2]} like this`
+  } else {
+    return `${people[0]}, ${people[1]} and ${people.length - 2} others like this`
+  }
+}
+console.log(likes(["Peter"]))
+console.log(likes(["Jacob", "Alex"]))
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]))
