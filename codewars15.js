8e1513c3d05b364288a5775f3ca99eba96f57dc6
Alexis Bustos <alexisbustos10@yahoo.com>
2025-07-26 12:39:04 -0700

    sum without highest and lowest

--- codewars15.js
+++ codewars15.js
@@ -22,5 +22,41 @@ const order = str => {
     return numA - numB;         // sort based on that number
   }).join(' ')
 }
-console.log(order("is2 Thi1s T4est 3a"))
-console.log(order("4of Fo1r pe6ople g3ood th5e the2"))
+// console.log(order("is2 Thi1s T4est 3a"))
+// console.log(order("4of Fo1r pe6ople g3ood th5e the2"))
+
+/*
+[8 kyu]
+Description:
+Task
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest 
+element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if 
+there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16 // because you exclude 1 and 10
+{ 1, 1, 11, 2, 3 } => 6 // because you exclude 1 and 11
+Input validation
+If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or 
+the given array is an empty list or a list with only 1 element, return 0.
+*/
+const sumExc = arr => {
+  if(arr == '' || arr == null) return 0
+  return arr.sort((a,b) => a - b).filter((e, i, arr) => {
+    if(i == 0){
+      return false
+    } else if(i == arr.length-1) {
+      return false
+    } else {
+      return true
+    }
+  }).reduce((acc, val) => acc + val, 0)
+}
+
+// let val = []
+console.log(sumExc([6, 2, 1, 8, 10]))
+console.log(sumExc([1, 1, 11, 2, 3]))
