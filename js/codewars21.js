0dc422b3f70e59646908f8bb8846790182205713
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-01 22:13:32 -0700

    convert kph to cms

--- js/codewars21.js
+++ js/codewars21.js
@@ -7,3 +7,22 @@ return a "Yes" string for true, or a "No" string for false.
 const boolToWord = (bool) => (bool ? "Yes" : "No");
 
 console.log(boolToWord(false));
+
+/*
+[8 kyu]
+Description:
+The cockroach is one of the fastest insects. Write a function which takes its speed 
+in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. 
+The result should be an Integer.
+*/
+
+const cockroachSpeed = (kph) => {
+  return Math.floor(kph * (100000 / 3600));
+};
+
+console.log(cockroachSpeed(1.08));
