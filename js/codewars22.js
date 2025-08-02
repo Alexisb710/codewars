ca2117911cf4332d5d11ef2d9043522131f64469
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-02 12:02:18 -0700

    powersOfTwo challenge completed

--- js/codewars22.js
+++ js/codewars22.js
@@ -14,4 +14,21 @@ move(3, 6) should equal 15
 const move = (position, roll) => {
   return position + roll * 2;
 };
-console.log(move(3, 6));
+// console.log(move(3, 6));
+
+/*
+[8 kyu]
+Description:
+Complete the function that takes a non-negative integer n as input, and 
+returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+*/
+const powersOfTwo = (n) => {
+  return Array.from({ length: n + 1 }, (_, i) => Math.pow(2, i));
+};
+
+console.log(powersOfTwo(1));
