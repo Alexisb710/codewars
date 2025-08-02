9d5d42373342a673c76b09383b88acdcc98c77d4
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-01 22:02:43 -0700

    completed boolToWord codewars challenge

---
+++ js/codewars21.js
@@ -0,0 +1,9 @@
+/*
+[8 kyu]
+Description:
+Complete the method that takes a boolean value and 
+return a "Yes" string for true, or a "No" string for false.
+*/
+const boolToWord = (bool) => (bool ? "Yes" : "No");
+
+console.log(boolToWord(false));
